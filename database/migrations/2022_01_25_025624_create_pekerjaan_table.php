96cdc59c42611c773c2b34b7781ff07fdea0aa77
Alvin MD <56709619+Suzukaze1@users.noreply.github.com>
2022-01-25 15:17:33 +0700

    migrasi tb pendidikan, pekerjaam, kewarganegaraaan, jenis identitas, suku

---
+++ database/migrations/2022_01_25_025624_create_pekerjaan_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePekerjaanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pekerjaan', function (Blueprint $table) {
+            $table->string('kode', 10);
+            $table->string('nama', 255);
+            $table->char('aktif', 1);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pekerjaan');
+    }
+}
